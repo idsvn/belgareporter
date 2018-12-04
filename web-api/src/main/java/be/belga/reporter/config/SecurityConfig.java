16df1399436280685a704a750b55a8c0d19a12f8
NhatLe0804 <chua.demonboy1996@gmail.com>
2018-12-04 14:09:02 +0700

    update

--- web-api/src/main/java/be/belga/reporter/config/SecurityConfig.java
+++ web-api/src/main/java/be/belga/reporter/config/SecurityConfig.java
@@ -1,71 +1,58 @@
 package be.belga.reporter.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
-@EnableWebSecurity(debug = true)
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+    @Autowired
+    UserDetailsService userDetailsService;
 
-    @Bean
+    @SuppressWarnings("deprecation")
     @Override
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(NoOpPasswordEncoder.getInstance());
     }
+
+    /*@Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests()
+                .antMatchers("/dashboard/**").hasAnyAuthority("ADMIN")
+                .antMatchers("/api/**").permitAll().and().csrf().disable().formLogin().loginPage("/login")
+                .loginProcessingUrl("/process_login").defaultSuccessUrl("/dashboard", true).failureUrl("/login?error=true").usernameParameter("username")
+                .passwordParameter("password").permitAll().and().logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/login")
+                .and().exceptionHandling().accessDeniedPage("/403");
+
+    }*/
     
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-    	http.authorizeRequests().anyRequest().permitAll().and().csrf().disable();
-  }
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().requireCsrfProtectionMatcher(new AntPathRequestMatcher("**/login")).and().authorizeRequests()
+				.antMatchers("/dashboard").hasRole("ADMIN").and().formLogin().defaultSuccessUrl("/dashboard")
+				.loginPage("/login").and().logout().permitAll();
+	}
 
+	@Bean
+	public AuthenticationManager customAuthenticationManager() throws Exception {
+		return authenticationManager();
+	}
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/*.css");
+		web.ignoring().antMatchers("/*.js");
+	}
 }
-
-// import javax.annotation.Resource;
-//
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.security.authentication.AuthenticationManager;
-// import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-// import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-// import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-// import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-// import org.springframework.security.core.userdetails.UserDetailsService;
-// import org.springframework.security.oauth2.provider.token.TokenStore;
-// import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
-//
-// @Configuration
-// @EnableWebSecurity
-// @EnableGlobalMethodSecurity(prePostEnabled = true)
-// public class SecurityConfig extends WebSecurityConfigurerAdapter {
-//
-// @Resource(name = "userDetailsService")
-// private UserDetailsService userDetailsService;
-//
-// @Override
-// @Bean
-// public AuthenticationManager authenticationManagerBean() throws Exception {
-// return super.authenticationManagerBean();
-// }
-//
-// @Autowired
-// public void globalUserDetails(AuthenticationManagerBuilder auth) throws Exception {
-// auth.userDetailsService(userDetailsService);
-// }
-//
-// @Bean
-// public TokenStore tokenStore() {
-// return new InMemoryTokenStore();
-// }
-//
-// // @Override
-// // protected void configure(HttpSecurity http) throws Exception {
-// //
-// // http.authorizeRequests().anyRequest().permitAll().and().csrf().disable();
-// // }
-//
-// }
---
+++ web-api/src/main/java/be/belga/reporter/config/SpringSecurityInitializer.java
@@ -0,0 +1,7 @@
+package be.belga.reporter.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
